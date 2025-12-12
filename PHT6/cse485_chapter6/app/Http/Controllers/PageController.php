588a568dd17b0e094c1e1986ad72254475c25b14
Ngoc Luong <ngocluongg0101@gmail.com>
2025-12-12 10:52:37 +0700

    PHT6

--- PHT6/cse485_chapter6/app/Http/Controllers/PageController.php
+++ PHT6/cse485_chapter6/app/Http/Controllers/PageController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    // TODO 8: Thêm phương thức
-    public function showHomepage(){
+    // TODO 8: Thêm phương thức này
+    public function showHomepage()
+    {
         // TODO 9: Thay vì echo, chúng ta 'return'
         return "Chào mừng bạn đến với PHT Chương 6 - Laravel!";
     }
--- PHT6/cse485_chapter6/routes/web.php
+++ PHT6/cse485_chapter6/routes/web.php
@@ -2,5 +2,6 @@
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
+// TODO 12: Thêm 2 route này
 Route::get('/', [PageController::class, 'showHomepage']);
 Route::get('/about', [PageController::class, 'showHomepage']);
